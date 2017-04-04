e06ed3281ae2d1e1847ba8e6763dc3d84f10b26b
Madeleine Sands <maddy.sands28@gmail.com>
2017-04-04 12:33:54 +0100

    start to working filters, added filters option, usernames to images

--- scripts/findFoodTwo.js
+++ scripts/findFoodTwo.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
+    var dataPass;
     getNotifications();
     $.ajax({
         url: "/getPosts",
         type: "GET",
         dataType: "json",
         success: function(data){
+        
             var dataReturned = JSON.parse(JSON.stringify(data));
-            
+            dataPass = dataReturned;
+          
             for(var property=0; property < dataReturned.length; property++) {
                 
                 var size = 0;
@@ -19,17 +22,18 @@ $(document).ready(function(){
                 var distance = "";
                 var lat = 0;
                 var long = 0;
+                var username = " ";
                 for (var l in x){
                     size++;
-                    if(size == 2){
+                    if(l == "title"){
                         title = x[l].toString();
                     } 
-                    else if (size == 3){
+                    else if (l == "description"){
                         description = x[l].toString();
                        // var y = size.toString();
                        // var p2 = p2 + ("<td>" + x[l] + "</td>");
                     };
-                    if (size == 4){
+                    if (l == "image"){
                         img = x[l].toString();
                         img = '<img src="' + img + '">';
                         
@@ -38,11 +42,14 @@ $(document).ready(function(){
                             img = '<div class = "text-center"><br><span class="glyphicon glyphicon-picture" >  </span></div>'
                         }
                     };
-                    if(size == 5){
+                    if(l == "latitude"){
                         lat = x[l];
                     }
-                    if(size == 6){
+                    if(l == "longitude"){
                         long = x[l];
+                    }
+                    if (l == "username"){
+                        username = x[l];
                     }
                 };
                 memberLang = 0;
@@ -59,14 +66,10 @@ $(document).ready(function(){
                 }
                 dist = getDistanceFromLatLonInKm(memberLang, memberLong, lat, long).toFixed(1);
                 
-                p2 = '<div class="w3-card-4">' + img + '<div class="w3-container w3-center"><h4>' + title + '</h4><p>' + description + '</p></div></div><br>'; 
-                
+                p2 = '<div class="w3-card-4">' + img + '<div class="w3-container w3-center"><h4>' + title + '</h4><p>' + description + '<br><small class = "text-muted"><i>' + username + '</i></small></p></div></div><br>'; 
+            
                 colNum = property;
                 colNum = colNum % 3;
-                //colNum = ((property) % 4);
-                //alert(property);
-               // alert("here" + colNum.toString());
-                
                 var str = "column" + colNum.toString();
               
                document.getElementById(str).insertAdjacentHTML('beforeEnd', p2);   
@@ -75,11 +78,127 @@ $(document).ready(function(){
     })
     $("#btnUpdate").click(function(){
         // TODO
-        refreshTable(dataset);
+        refreshTable(dataPass);
     })
  
     
 });
+
+function refreshTable(data){
+    var data = data;
+    filters = [];
+    filters = loadFilters();
+    reloadTable(filters, data);
+}
+
+
+function loadFilters(){
+    var filters = new Object();
+    //var chkMealType = $("#collapseMealType")[0];
+   // alert(chkMealType.value.toString());
+    filters.mealType = []
+    $("#collapseMealType input:checked").each(function(){
+        filters.mealType.push($(this).attr('name'));
+        
+    })
+    return filters;
+}   
+
+function reloadTable(filters, data){
+    $("#column0").html("");
+     $("#column1").html("");
+     $("#column2").html("");
+
+    
+    var dataReturned = data;
+    var numOfYes =-1;
+    for(var property=0; property < dataReturned.length; property++) {
+                
+                var size = 0;
+                var x = dataReturned[property];
+                var title = "";
+                var description = "";
+                var img = "";
+                var distance = "";
+                var lat = 0;
+                var long = 0;
+                var username = " ";
+                var totalShow = true;
+                for (var l in x){
+                    size++;
+                    if (!("mealtype" in x)){
+                        totalShow = false;
+                    }
+                    if (l == "mealtype"){
+                        var show = 0;
+                        for (var choice in filters.mealType){
+                            if (x[l] == filters.mealType[choice]){
+                                show++;
+                            }
+                        }
+                        if (show == 0){
+                            totalShow  = false;;
+                        }
+                        
+                    }
+                    if(l == "title"){
+                        title = x[l].toString();
+                    } 
+                    else if (l == "description"){
+                        description = x[l].toString();
+                       // var y = size.toString();
+                       // var p2 = p2 + ("<td>" + x[l] + "</td>");
+                    };
+                    if (l == "image"){
+                        img = x[l].toString();
+                        img = '<img src="' + img + '">';
+                        
+                        if (img == '<img src="">'){
+                   
+                            img = '<div class = "text-center"><br><span class="glyphicon glyphicon-picture" >  </span></div>'
+                        }
+                    };
+                    if(l == "latitude"){
+                        lat = x[l];
+                    }
+                    if(l == "longitude"){
+                        long = x[l];
+                    }
+                    if (l == "username"){
+                        username = x[l];
+                    }
+                };
+                memberLang = 0;
+                memberLong = 0;
+                if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(function(pos) {
+                            memberLang = pos.coords.latitue;
+                            memberLong = pos.coords.longitude;
+                        });
+                    } else {
+                        memberLang = 54.775250;
+                        memberLong = -1.584852;
+                        alert("ERROR : Using default location");
+                }
+                dist = getDistanceFromLatLonInKm(memberLang, memberLong, lat, long).toFixed(1);
+                if (totalShow == true){
+                   
+                    numOfYes++;
+                    p2 = '<div class="w3-card-4">' + img + '<div class="w3-container w3-center"><h4>' + title + '</h4><p>' + description + '<br><small class = "text-muted"><i>' + username + '</i></small></p></div></div><br>'; 
+
+                    colNum = numOfYes;
+                    alert(colNum);
+                    colNum = colNum % 3;
+                    var str = "column" + colNum.toString();
+
+                   document.getElementById(str).insertAdjacentHTML('beforeEnd', p2);
+                }
+            } 
+        
+     
+}
+    
+
 
 // TODO eventually move notifications into global functions? 
 // TODO has to loop every noe and then. Set timer??
