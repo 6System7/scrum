082d3f6f28b9f9a1c5260da03b6072d602716886
mikecroall <mikebcroall@gmail.com>
2017-04-11 15:33:52 +0100

    Fix internal server error on edit // Tidy up

--- scripts/postfood.js
+++ scripts/postfood.js
@@ -1,27 +1,3 @@
-var dataURLToBlob = function(dataURL) {
-    var BASE64_MARKER = ';base64,';
-    if (dataURL.indexOf(BASE64_MARKER) == -1) {
-        var parts = dataURL.split(',');
-        var contentType = parts[0].split(':')[1];
-        var raw = parts[1];
-
-        return new Blob([raw], {
-            type: contentType
-        });
-    }
-    var parts = dataURL.split(BASE64_MARKER);
-    var contentType = parts[0].split(':')[1];
-    var raw = window.atob(parts[1]);
-    var rawLength = raw.length;
-    var uInt8Array = new Uint8Array(rawLength);
-    for (var i = 0; i < rawLength; ++i) {
-        uInt8Array[i] = raw.charCodeAt(i);
-    }
-    return new Blob([uInt8Array], {
-        type: contentType
-    });
-}
-
 function validInputs() {
     // TODO Maddy? - nicer alerts in this section?
 
@@ -69,7 +45,9 @@ function sendPostData() {
         });
 
         indexedArray.image = $("#imgPreview").attr("src");
-        indexedArray.saveImage = imageSelected;
+        if (imageSelected) {
+            indexedArray.saveImage = true;
+        }
         // $("#imgPreview").attr("src", "");
 
         if (marker) {
@@ -164,7 +142,6 @@ function previewFile() {
             canvas.height = height;
             canvas.getContext('2d').drawImage(image, 0, 0, width, height);
             var dataUrl = canvas.toDataURL('image/jpeg');
-            // var resizedImage = dataURLToBlob(dataUrl);
             preview.attr("src", dataUrl);
         }
         image.src = reader.result;
